313c16d9ded9799957494db080608ec04817f8ed
Jelani Jackson <jeljack86@gmail.com>
2019-04-26 15:57:33 -0400

    Updated tests to be pytests and next to code

---
+++ deletesite/utilities/tests/test_directory.py
@@ -0,0 +1,44 @@
+"""Test file for directory utilities
+"""
+
+from deletesite.utilities.directory import ensure_exists
+
+from os import path
+
+directory_module = 'deletesite.utilities.directory'
+
+
+def test_creating_directory_if_none_present(mocker):
+    """Test asserts directory does not exist then tests that the
+    `ensure_exists` method creates the directory and removes the
+    directory in cleanup
+    """
+
+    dir_path = 'test'
+    file_path = path.join(dir_path, 'test.log')
+
+    mocked_makedirs = mocker.patch(f'{directory_module}.makedirs')
+    mocked_dirname = mocker.patch(f'{directory_module}.path.dirname')
+    mocked_exists = mocker.patch(f'{directory_module}.path.exists')
+
+    mocked_dirname.return_value = dir_path
+    mocked_exists.return_value = False
+
+    ensure_exists(file_path)
+
+    mocked_makedirs.assert_called_once_with(dir_path)
+
+
+def test_should_not_error_when_attempting_to_create_an_existing_directory(mocker):
+    """Test asserts that directory `ensure_exists` method does not error if
+    directory already exists
+    """
+
+    mocked_makedirs = mocker.patch(f'{directory_module}.makedirs')
+    mocked_exists = mocker.patch(f'{directory_module}.path.exists')
+
+    mocked_exists.return_value = True
+
+    ensure_exists('file path')
+
+    mocked_makedirs.assert_not_called()
