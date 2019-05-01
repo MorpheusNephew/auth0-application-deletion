70617f9d1c33dc9bcc30014f065836a773b843b4
Jelani Jackson <jeljack86@gmail.com>
2019-04-30 19:46:33 -0500

    Updated __main__.py

--- deletesite/__main__.py
+++ deletesite/__main__.py
@@ -1,11 +1,11 @@
 """Main file that will be run when the command `python deletesite` is ran
 """
 
-from settings import load_settings
+from .settings import load_settings
 
 if __name__ == "__main__":
     load_settings()
 
-    name = input("What is your name?")
+    name = input("What is your name? ")
 
     print(f"Hello {name}")
