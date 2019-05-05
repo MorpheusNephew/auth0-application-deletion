6bf55cb537a710e8e777833af9a2be40606002f3
Jelani Jackson <jeljack86@gmail.com>
2019-05-05 15:35:18 -0500

    Rmoved logger instantiation

--- deletesite/utilities/settings.py
+++ deletesite/utilities/settings.py
@@ -1,12 +1,8 @@
 from dotenv import load_dotenv
-from deletesite.loggers import Auth0Logger
-import os
 
 
 def load_settings():
     """Loads environment variables from .env file to running environment
-    and initializes the logger
     """
 
     load_dotenv()
-    Auth0Logger(os.getenv('AUTH0_LOG_PATH', None))
