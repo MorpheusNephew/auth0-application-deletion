e3a8d759301bc14b768b5ec7e32c6cb314b6b7be
Jelani Jackson <jeljack86@gmail.com>
2019-05-05 18:13:28 -0500

    Running auth0 manager without variable

--- __main__.py
+++ __main__.py
@@ -8,9 +8,7 @@ from deletesite.manager import Auth0ManagerFactory
 def main():
     load_settings()
 
-    auth0_manager = Auth0ManagerFactory.get_auth0_manager()
-
-    auth0_manager.run()
+    Auth0ManagerFactory.get_auth0_manager().run()
 
 
 if __name__ == "__main__":
