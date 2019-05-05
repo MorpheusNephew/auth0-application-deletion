fc0fa3f25689b5b3e45204a73e45e605b9da0dd8
Jelani Jackson <jeljack86@gmail.com>
2019-05-05 15:35:41 -0500

    Created auth0 logger factory to return instance of logger

---
+++ deletesite/factories/loggerfactory.py
@@ -0,0 +1,17 @@
+from deletesite.loggers import Auth0Logger
+import os
+
+
+class Auth0LoggerFactory:
+    """Factory to get logger
+    """
+
+    @staticmethod
+    def get_logger():
+        """Returns logger
+
+        Returns:
+            Auth0Logger -- logger to be used for logging
+        """
+
+        return Auth0Logger(os.getenv('AUTH0_LOG_PATH', None))
