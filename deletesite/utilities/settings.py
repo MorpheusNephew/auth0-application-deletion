c8d28fe758c9dc579c3a9e05351d216eaa0cddce
Jelani Jackson <jeljack86@gmail.com>
2019-05-05 14:29:34 -0500

    Initializing logger when settings are loaded

--- deletesite/utilities/settings.py
+++ deletesite/utilities/settings.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
+from deletesite.loggers import Auth0Logger
 
 
-def load_settings():
+def load_settings(log_path=None):
     """Loads environment variables from .env file to running environment
+    and initializes the logger
     """
 
     load_dotenv()
+    Auth0Logger(log_path)
