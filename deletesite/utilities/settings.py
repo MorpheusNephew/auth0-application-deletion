37a4aaa6eba2876d94c66619fa6d80d3960202a8
Jelani Jackson <jeljack86@gmail.com>
2019-05-05 14:33:06 -0500

    Reading log patth from environment

--- deletesite/utilities/settings.py
+++ deletesite/utilities/settings.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 from deletesite.loggers import Auth0Logger
+import os
 
 
-def load_settings(log_path=None):
+def load_settings():
     """Loads environment variables from .env file to running environment
     and initializes the logger
     """
 
     load_dotenv()
-    Auth0Logger(log_path)
+    Auth0Logger(os.getenv('AUTH0_LOG_PATH', None))
