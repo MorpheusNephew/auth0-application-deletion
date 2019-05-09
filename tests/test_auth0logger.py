f846dd8a747537e8ead7bccaf2b7c8e63805a744
Jelani Jackson <jeljack86@gmail.com>
2019-05-09 06:50:32 -0500

    Filled out logging tests, but must figure out why mocking Stream handler causes an error

--- tests/test_auth0logger.py
+++ tests/test_auth0logger.py
@@ -1,6 +1,9 @@
-# from deletesite.loggers import Auth0Logger
+from logging import Logger
+from uuid import uuid4
 
-auth0_logger_module = 'deletesite.loggers.logger'
+from deletesite.loggers import Auth0Logger
+
+_logging_module = 'deletesite.loggers.logger.logging'
 
 
 class TestLogger:
@@ -12,15 +15,41 @@ class TestLogger:
         for the logger if no output file path is provided
 
         Arguments:
-            mocker {pytest_mocker} -- wrapper for python mock
+            mocker {pytest.mocker} -- wrapper for python mock
         """
 
-        # TODO: Figure out mocking strategy for instance property of a nested class
+        mocked_logger_addHandler = mocker.patch.object(Logger, 'addHandler')
+        # mocked_logger_streamHandler = mocker.patch(
+        #     f'{_logging_module}.StreamHandler')
+        mocked_logger_fileHandler = mocker.patch(
+            f'{_logging_module}.FileHandler')
 
-        # mocked_logger = mocker.patch(f'{auth0_logger_module}.logging.Logger')
+        Auth0Logger._Auth0LoggerInstance(output_file_path=None)
 
-        # Auth0Logger._Auth0LoggerInstance(output_file_path=None)
+        mocked_logger_addHandler.assert_called_once()
+        # Figure out: AttributeError: 'LogCaptureHandler' object has no attribute 'stream'
+        # mocked_logger_streamHandler.assert_called_once()
+        mocked_logger_fileHandler.assert_not_called()
 
-        # mocked_logger.addHandler.assert_called_once()
+    def test_add_log_handlers_with_output_file_path(self, mocker):
+        """Ensuring that file handler is added to the list of handlers
+        for logging if an input file path is provided
 
-        pass
+        Arguments:
+            mocker {pytest.mocker} -- wrapper for python mock
+        """
+
+        mocked_logger_addHandler = mocker.patch.object(Logger, 'addHandler')
+        # mocked_logger_streamHandler = mocker.patch(
+        #     f'{_logging_module}.StreamHandler')
+        mocked_logger_fileHandler = mocker.patch(
+            f'{_logging_module}.FileHandler')
+
+        file_path = f'{str(uuid4())}.log'
+
+        Auth0Logger._Auth0LoggerInstance(output_file_path=file_path)
+
+        assert mocked_logger_addHandler.call_count == 2
+        # Figure out: AttributeError: 'LogCaptureHandler' object has no attribute 'stream'
+        # mocked_logger_streamHandler.assert_called_once()
+        mocked_logger_fileHandler.assert_called_once()
